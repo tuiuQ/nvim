a9138631b70ced73470de8fed1e18900001f71cd
tuiu <13719283454@163.com>
2024-03-07 11:40:13 +0800

    安装 treesitter

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -15,5 +15,6 @@ vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappin
 
 require("lazy").setup({
 	require("plugins.nvim-tree"),
-	require("plugins.colorscheme")
+	require("plugins.colorscheme"),
+	require("plugins.treesitter")
 })
