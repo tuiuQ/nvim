0bb1a0551c914f8db1c0790544063dd8b4336033
tuiu <13719283454@163.com>
2024-03-08 16:29:43 +0800

    nvim: configure options

--- init.lua
+++ init.lua
@@ -1,15 +1,1 @@
 require("core")
-local keymap = vim.keymap.set
-
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.termguicolors = true
-vim.opt.termguicolors = true
-vim.o.tabstop = 2
-vim.o.softtabstop = 2
-vim.o.shiftwidth = 2
-vim.o.clipboard = "unnamedplus"
-vim.opt.clipboard = "unnamedplus"
-vim.g.clipboard = "unnamedplus"
-
-
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -32,6 +32,7 @@ M.default_options = {
 	tabstop = 2, -- insert 2 spaces for a tab
 	cursorline = true, -- highlight the current line
 	number = true, -- set numbered lines
+  relativenumber = true,
 	numberwidth = 4, -- set number column width to 2 {default 4}
 	signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
 	wrap = false, -- display lines as one long line
