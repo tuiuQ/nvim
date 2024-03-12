1ef43e63b91c1155d0c1af5a484a8fa068208fb0
tuiu <13719283454@163.com>
2024-03-12 10:22:18 +0800

    use ufo plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -104,17 +104,5 @@ return {
   {
     "mg979/vim-visual-multi",
   },
-  {
-    'jghauser/fold-cycle.nvim',
-    config = function()
-      vim.o.foldcolumn = '1' -- '0' is not bad
-      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-      vim.o.foldlevelstart = 99
-      vim.o.foldenable = true
-
-      utils.keymap.load("foldcycle")
-
-      require('fold-cycle').setup()
-    end
-  }
+  ufoPluginConfig,
 }
