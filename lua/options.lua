92515c3d6c8dcc7a5bf6fc7c91fce564f99f6731
tuiu <13719283454@163.com>
2025-07-16 19:35:35 +0800

    enable fold

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,7 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.lua.treesitter.foldexpr()"
+vim.opt.foldenable = false
